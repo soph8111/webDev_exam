743c6162b2afb9dec08e1487b8002f61db91f99c
Sophie Spang <soph8111@stud.kea.dk>
2022-09-25 12:34:13 +0200

    footer

--- comp_footer.php
+++ comp_footer.php
@@ -1,5 +1,67 @@
     <footer>
-        footer
+        
+    <div class="footer_links">
+        <div class="company footer_subject">
+            <h6>Company</h6>
+            <ul class="navigation_links">
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/about/careers">Careers</a>
+                </li>
+                <li>
+                    <a href="/mobile">Mobile</a>
+                </li>
+                <li>
+                    <a href="https://www.momondo.co.uk/discover">Discover</a>
+                </li>
+                <li>
+                    <a href="/company">How we work</a>
+                </li>
+                <li>
+                    <a href="/about/momondo-coupon-codes">Coupon codes</a>
+                </li>
+            </ul>
+        </div>
+        <div class="contact footer_subject">
+            <h6>Contact</h6>
+            <ul class="navigation_links">
+                <li>
+                    <a href="/help">Help/FAQ</a>
+                </li>
+                <li>
+                    <a href="/press-contact">Press</a>
+                </li>
+                <li>
+                    <a href="https://affiliates.kayak.com">Affiliates</a>
+                </li>
+            </ul>
+        </div>
+        <div class="more footer_subject">
+            <h6>More</h6>
+            <ul class="navigation_links">
+                <li>
+                    <a href="/airline-fees">Airline fees</a>
+                </li>
+                <li>
+                    <a href="/airlines">Airlines</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="privacy_menu mgt_2">
+        <div class="privacy_menu_link">
+            <a href="/privacy">Privacy</a>
+            <a href="/terms-of-use">Terms &amp; Conditions</a>
+            <a href="/about/impressum">Imprint</a>
+        </div>
+        <span class="privacy_menu_copyright">© 2022 Momondo</span>
+    </div>
+
+
+
     </footer>
 
     <script src="js_app"></script>
